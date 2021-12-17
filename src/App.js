2a973ac93dc7d6fee8f217b9128c9a18991fe579
David Conley <dconley1989@gmail.com>
2021-12-17 11:29:12 -0700

    added state and effect functionality as well as onchange and onsubmit so that form can collect data and display it

--- src/App.js
+++ src/App.js
@@ -1,30 +1,98 @@
-import React from "react";
-import { Link, Route, Switch } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Pizza from "./Pizza";
 import OrderForm from "./OrderForm";
+import formSchema from "./formSchema";
+import * as yup from "yup";
+import axios from "axios";
+
+const initialOrderInfo = {
+  name: "",
+  size: "",
+  peperoni: false,
+  onions: false,
+  peppers: false,
+  olives: false,
+  special: "",
+};
+
+const initialFormErrors = {
+  name: "",
+};
+
+const initialDisabled = true;
+
+const newOrders = [];
 
 const App = () => {
+  const [orderInfo, setOrderInfo] = useState(initialOrderInfo);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+  const [orders, setOrders] = useState(newOrders);
+
+  const validateOrder = (name, value) => {
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: "" }))
+      .catch((err) => setFormErrors({ ...formErrors, [name]: err.errors[0] }));
+  };
+
+  const orderChange = (name, value) => {
+    validateOrder(name, value);
+    setOrderInfo({ ...orderInfo, [name]: value });
+  };
+
+  const postOrder = (newOrder) => {
+    axios
+      .post("https://reqres.in/api/orders", newOrder)
+      .then((response) => {
+        console.log(response);
+        setOrders([...orders, response.data]);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setOrderInfo(initialOrderInfo));
+  };
+
+  const orderSubmit = () => {
+    const newOrder = {
+      name: orderInfo.name.trim(),
+      size: orderInfo.size,
+      peperoni: orderInfo.peperoni,
+      onions: orderInfo.onions,
+      peppers: orderInfo.peppers,
+      olives: orderInfo.olives,
+      special: orderInfo.special.trim(),
+    };
+
+    postOrder(newOrder);
+  };
+
+  useEffect(() => {
+    formSchema.isValid(orderInfo).then((valid) => setDisabled(!valid));
+  });
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/pizza">Order Now</Link>
-      </div>
+    <div>
       <Switch>
         <Route>
           <Pizza path="/pizza/id" />
         </Route>
         <Route path="/pizza">
-          <OrderForm />
+          <OrderForm
+            orderInfo={orderInfo}
+            orderChange={orderChange}
+            orderSubmit={orderSubmit}
+            errors={formErrors}
+            disabled={disabled}
+          />
         </Route>
         <Route exact path="/">
           <Home />
         </Route>
       </Switch>
-    </>
+    </div>
   );
 };
 export default App;
--- src/OrderForm.js
+++ src/OrderForm.js
@@ -1,57 +1,94 @@
 import React from "react";
 
-export default function pizzaForm() {
+export default function pizzaForm({
+  orderInfo,
+  orderChange,
+  orderSubmit,
+  disabled,
+  errors,
+}) {
+  const handleChange = (evt) => {
+    const { name, value, checked, type } = evt.target;
+    const valueProvided = type === "checkbox" ? checked : value;
+    orderChange(name, valueProvided);
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    orderSubmit();
+  };
+
   return (
     <div className="order-form">
       <h2>Build your Pizza</h2>
-      <form>
-          <label> Name:
-              <input
-              id='name-input'
-              type='text'
-              name='name'
-              value={}
-              />
-          </label>
-          <label>Pizza Size:
-              <select name='size' value={} >
-                  <option value='Small'>Small</option>
-                  <option value='Medium'>Medium</option>
-                  <option value='Large'>Large</option>
-                  <option value='Extra-Large'>Extra-Large</option>
-              </select>
-          </label>
-          <label>Peperoni:
-              <input 
-              type='checkbox'
-              name='peperoni'
-              checked={}
-              />
-          </label>
-          <label>Onions:
-              <input
-              type='checkbox'
-              name='onions'
-              checked={}
-              />
-          </label>
-          <label>Green-peppers:
-              <input 
-              type='checkbox'
-              name='green-peppers'
-              checked={}
-              />
-          </label>
-          <label>
-              <input 
-              type='checkbox'
-              name='olives'
-              checked={}
-              />
-          </label>
-          <label>
-              <input type='text' name='Special' value={}/>
-          </label>
+      <form onSubmit={handleSubmit}>
+        <label>
+          {" "}
+          Name:
+          <input
+            id="name-input"
+            type="text"
+            name="name"
+            value={orderInfo.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Pizza Size:
+          <select name="size" value={orderInfo.size} onChange={handleChange}>
+            <option value="Small">Small</option>
+            <option value="Medium">Medium</option>
+            <option value="Large">Large</option>
+            <option value="Extra-Large">Extra-Large</option>
+          </select>
+        </label>
+        <label>
+          Peperoni:
+          <input
+            type="checkbox"
+            name="peperoni"
+            checked={orderInfo.peperoni}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Onions:
+          <input
+            type="checkbox"
+            name="onions"
+            checked={orderInfo.onions}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Green-peppers:
+          <input
+            type="checkbox"
+            name="peppers"
+            checked={orderInfo.peppers}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="olives"
+            checked={orderInfo.olives}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <input
+            type="text"
+            name="special"
+            value={orderInfo.special}
+            onChange={handleChange}
+          />
+        </label>
+        <button disabled={disabled}>Submit!</button>
+        <div className="errors">
+          <p>{errors.name}</p>
+        </div>
       </form>
     </div>
   );
